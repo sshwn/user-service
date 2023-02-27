29e86806606cdc0c791de797b0dd6fa33dfdc90a
hyunWoo <nhw0926@gmail.com>
2023-02-28 01:13:58 +0900

    [할일] #1 프로젝트 기본설정 - application.yml의 데이터를 java에서 사용하기 - H2 database 연동

--- src/main/java/com/example/userservice/UserServiceApplication.java
+++ src/main/java/com/example/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient    // MSA 유레카 서버에 등록하기 위해 추가
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
