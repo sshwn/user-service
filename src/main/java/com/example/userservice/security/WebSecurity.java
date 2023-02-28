20259e9036b9f6ba0525ce34fe9f7121a83dee75
hyunWoo <nhw0926@gmail.com>
2023-03-01 01:12:47 +0900

    [할일] #4 WebSecurityConfigurerAdapter가 Deprecated 되었으니 SecurityFilterChain를 Bean으로 등록해서 사용하도록 수정

--- src/main/java/com/example/userservice/security/WebSecurity.java
+++ src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,19 +1,21 @@
 package com.example.userservice.security;
 
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableDiscoveryClient
-public class WebSecurity extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class WebSecurity {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests().antMatchers("/users/**").permitAll();  // /users/** 로 호출되는 uri 값은 통과한다는 의미
 
         http.headers().frameOptions().disable(); // 추가하지 않으면 h2-console 접근 안됨
+        return http.build();
     }
 }
