6491c114238ed5c9b17dd7bdace63a392c90a4d6
nhw0926@gmail.com <nhw0926@gmail.com>
2023-05-10 02:24:46 +0900

    [할일] #11 RestTemplate 사용 - UserServiceApplication.java 에 선언한 getRestTemplate() 메서드에 @LoadBalanced를 선언하면 user-service.yml에 선언한 order_service.url 값에 주소가아닌 유레카에 등록된 프로젝트이름을 사용할 수 있다 ex) http://127.0.0.1:8000/order-service/%s/orders -> http://order-service/order-service/%s/orders

--- src/main/java/com/example/userservice/UserServiceApplication.java
+++ src/main/java/com/example/userservice/UserServiceApplication.java
@@ -3,8 +3,10 @@ package com.example.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient    // MSA 유레카 서버에 등록하기 위해 추가
@@ -18,5 +20,13 @@ public class UserServiceApplication {
 	public BCryptPasswordEncoder passwordEncoder() {
 		// 이 클래스안에 선언된 Bean을 메모리안에 추가한다.
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		// 반환시키고자하는 데이터의 종류를 restTemplate라고 명시함.
+		// @Bean 등록 시 Controller service등에서 사용할 수 있음
+		return new RestTemplate();
 	}
 }
--- src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -7,11 +7,16 @@ import com.example.userservice.vo.ResponseOrder;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +26,9 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService{
     private UserRepository userRepository;
     private BCryptPasswordEncoder passwordEncoder;
+
+    Environment env;
+    RestTemplate restTemplate;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -35,9 +43,11 @@ public class UserServiceImpl implements UserService{
     }
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, Environment env, RestTemplate restTemplate) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.env = env;
+        this.restTemplate = restTemplate;
     }
 
     @Override
@@ -65,8 +75,17 @@ public class UserServiceImpl implements UserService{
 
         UserDto userDto = new ModelMapper().map(userEntity, UserDto.class); // UserEntity값을 그냥 전달할 수 없기에 UserDto로 변환한다 ()
 
-        List<ResponseOrder> orders = new ArrayList<>();
-        userDto.setOrders(orders);
+//        List<ResponseOrder> orders = new ArrayList<>();
+        /* Using as rest template */
+        String orderUrl = String.format(env.getProperty("order-service.url"), userId);  // apigateway  %s = 사용자 아이디
+        ResponseEntity<List<ResponseOrder>> orderListResponse =
+                restTemplate.exchange(orderUrl, HttpMethod.GET, null,
+                        new ParameterizedTypeReference<List<ResponseOrder>>() {   // List<ResponseOrder> 는 여기서 호출할 order-service 메서드의 반환값
+                });
+
+        List<ResponseOrder> ordersList = orderListResponse.getBody();
+
+        userDto.setOrders(ordersList);
 
         return userDto;
     }
